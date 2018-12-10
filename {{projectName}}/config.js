0289f9d9ef79c427a658c45f1edcd07c446aee63
Jon.Xia <xiajia@vip.qq.com>
2018-12-10 22:14:23 +0800

    完善配置文件的使用

--- {{projectName}}/config.js
+++ {{projectName}}/config.js
@@ -1,3 +1,7 @@
+module.exports.base = {
+  title: 'This is Function: {{projectName}}!'
+}
+module.exports.table = {}
 module.exports.oss = {}
 module.exports.logs = {}
 module.exports.nms = {}
--- {{projectName}}/index.js
+++ {{projectName}}/index.js
@@ -1,7 +1,7 @@
 const config = require('./config')
 
 const handler = ({{event === 'HTTP' ? 'req, rep, ctx' : 'context, callback'}}) => {
-  {{event === 'HTTP' ? "rep.send('Hello World')" : "callback(null, '')"}}
+  {{event === 'HTTP' ? "rep.send(config.base.title)" : "callback(null, config.base.title)"}}
 }
 
 const initializer = (context, callback) => {
